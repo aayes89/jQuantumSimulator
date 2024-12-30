d6bac6c120e0afa16f190dd19f264e0be2b2ace3
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2024-12-29 19:16:46 -0600

    Update HadamardGate.java
    Description added

--- src/quantum/gates/HadamardGate.java
+++ src/quantum/gates/HadamardGate.java
@@ -4,8 +4,12 @@ import quantum.QuantumGate;
 import quantum.QuantumState;
 
 /**
- *
  * @author Slam
+ * 
+ * Implementación de la puerta Hadamard (H Gate)
+ * 
+ * Operación: Coloca el qubit en una superposición equilibrada de |0> y |1>.
+ * Qubit objetivo: Se afecta su amplitud para dividir el estado entre + y -.
  */
 public class HadamardGate extends QuantumGate {
 
@@ -17,6 +21,28 @@ public class HadamardGate extends QuantumGate {
 
     @Override
     public void apply(QuantumState state) {
+        int size = (int) Math.pow(2, state.getNumQubits());
+        double[] real = state.getRealPart();
+        double[] imag = state.getImagPart();
+        int mask = 1 << targetQubit;
+
+        for (int i = 0; i < size; i++) {
+            if ((i & mask) == 0) {
+                int partner = i | mask;
+                double tempReal = (real[i] + real[partner]) / Math.sqrt(2);
+                double tempImag = (imag[i] + imag[partner]) / Math.sqrt(2);
+                double partnerReal = (real[i] - real[partner]) / Math.sqrt(2);
+                double partnerImag = (imag[i] - imag[partner]) / Math.sqrt(2);
+
+                real[i] = tempReal;
+                imag[i] = tempImag;
+                real[partner] = partnerReal;
+                imag[partner] = partnerImag;
+            }
+        }
+    }
+
+    public void apply1(QuantumState state) {
         double[] real = state.getRealPart();
         double[] imag = state.getImagPart();
         int[] targets = calculateTargets(targetQubit, state.getNumQubits());
